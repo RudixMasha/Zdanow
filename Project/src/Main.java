f039b6cb0335054306816b5e0404ebb8913fefb0
KateBochaeva <kbochaewa@yandex.ru>
2023-07-17 12:39:30 +0300

    q

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,3 +1,6 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
@@ -13,18 +16,18 @@ public class Main {
         JButton startButton = new JButton("Начать");
         JButton exitButton = new JButton("Выйти");
 
-        ImageIcon icon = new ImageIcon("kva.jpg");
-        Image image = icon.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+        ImageIcon icon = new ImageIcon("kvaa.png");
+        Image image = icon.getImage().getScaledInstance(500, 400, Image.SCALE_SMOOTH);
         Icon scaledIcon = new ImageIcon(image);
 
         JLabel imageLabel = new JLabel(scaledIcon);
         frame.add(imageLabel, BorderLayout.NORTH);
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 10));
+        JPanel buttonPanel = new JPanel();
         buttonPanel.add(exitButton);
-        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.add(buttonPanel, BorderLayout.CENTER);
 
-        JPanel startButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        JPanel startButtonPanel = new JPanel(new GridLayout(1, 2));
         startButtonPanel.add(startButton);
         frame.add(startButtonPanel, BorderLayout.SOUTH);
 
@@ -34,9 +37,19 @@ public class Main {
 
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(1, 3));
-        JButton yesButton = new JButton("Да");  // Переименовали кнопку
-        panel.add(yesButton);                  // Добавили новую кнопку
+        JButton yesButton = new JButton("Знаю");
+        panel.add(yesButton);          // Добавили новую кнопку
         JButton checkButton = new JButton("Проверить");
+
+        try {
+            File soundFile = new File("ost.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         checkButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
@@ -63,7 +76,7 @@ public class Main {
             }
         });
         panel.add(checkButton);
-        JButton noButton = new JButton("Нет");  // Добавили новую кнопку
+        JButton noButton = new JButton("Не знаю");  // Добавили новую кнопку
         noButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
@@ -201,25 +214,36 @@ public class Main {
                 System.exit(0);
             }
         });
-        Border border = BorderFactory.createLineBorder(new Color(154, 205, 50, 165),5);
+        Border border = BorderFactory.createLineBorder(new Color(0, 100, 0, 165),7);
         Border border2 = BorderFactory.createLineBorder(new Color(255, 255, 115, 147),5);
         frame.add(panel, BorderLayout.SOUTH);
         panel.setVisible(false);
         frame.add(startButton, BorderLayout.WEST);
         frame.add(exitButton, BorderLayout.EAST);
-        frame.setSize(362, 300);
+        //frame.setSize(500, 500);
+        frame.setBounds(460,180,500,500);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBackground(new Color(34, 139, 34));// не работает лмао
-        exitButton.setBackground(new Color(255, 255, 115)); exitButton.setForeground(new Color(0, 100, 0));
+        frame.getContentPane().setBackground(new Color(235, 255, 84));// не работает лмао
+        exitButton.setBackground(new Color(255, 255, 170)); exitButton.setForeground(new Color(0, 100, 0));
         exitButton.setBorder(border);
-        startButton.setBackground(new Color(255, 255, 115)); startButton.setForeground(new Color(0, 100, 0));
+        exitButton.setPreferredSize(new Dimension(246, 30));
+        exitButton.setFont(new Font("Arial", Font.BOLD, 20));
+        startButton.setBackground(new Color(255, 255, 170)); startButton.setForeground(new Color(0, 100, 0));
         startButton.setBorder(border);
+        startButton.setPreferredSize(new Dimension(246, 30));
+        startButton.setFont(new Font("Arial", Font.BOLD, 20));
         yesButton.setBackground(new Color(157, 206, 58)); yesButton.setForeground(new Color(255, 250, 250));
         yesButton.setBorder(border2);
+        yesButton.setPreferredSize(new Dimension(150, 50));
+        yesButton.setFont(new Font("Arial", Font.BOLD, 20));
         noButton.setBackground(new Color(157, 206, 58)); noButton.setForeground(new Color(255, 250, 250));
         noButton.setBorder(border2);
+        noButton.setFont(new Font("Arial", Font.BOLD, 20));
         checkButton.setBackground(new Color(255, 255, 115)); checkButton.setForeground(new Color(0, 100, 0));
         checkButton.setBorder(border);
+        checkButton.setFont(new Font("Arial", Font.BOLD, 20));
+        cardLabel.setFont(new Font("Arial", Font.BOLD, 25));
+        cardLabel.setForeground(new Color(0, 100, 0));
     }
 }
